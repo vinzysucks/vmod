7182d63f58071a4143fd4afa0830cb4499d00a27
vinzy <136903632+vinzysucks@users.noreply.github.com>
2025-08-11 23:19:12 -0600

    paste sign blendy fix

--- src/main/java/me/vinzy/vmod/utils/ModuleManager.java
+++ src/main/java/me/vinzy/vmod/utils/ModuleManager.java
@@ -5,8 +5,16 @@ import me.vinzy.vmod.VMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiEditSign;
+import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.ChatComponentText;
 import org.lwjgl.input.Keyboard;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.lang.reflect.Field;
 
 public class ModuleManager {
 
@@ -33,6 +41,28 @@ public class ModuleManager {
                     VMod.PREFIX + "Toggled touchscreen."));
         } else if (key == ConfigFile.printNbt_Key) {
             GuiUtils.printNBT();
+        } else if (key == Keyboard.KEY_V && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+            GuiScreen currentScreen = Minecraft.getMinecraft().currentScreen;
+            if (currentScreen instanceof GuiEditSign) {
+                GuiEditSign signGui = (GuiEditSign) currentScreen;
+                try {
+                    Field titleSignField = signGui.getClass().getDeclaredField("tileSign");
+                    Field editLineField = signGui.getClass().getDeclaredField("editLine");
+                    titleSignField.setAccessible(true);
+                    editLineField.setAccessible(true);
+                    String lineContent = ((TileEntitySign)titleSignField.get(signGui)).signText[(int)editLineField.get(signGui)].getFormattedText();
+                    String clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+                    ((TileEntitySign)titleSignField.get(signGui)).signText[(int)editLineField.get(signGui)] = new ChatComponentText(lineContent+clipboard);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (UnsupportedFlavorException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
     }
