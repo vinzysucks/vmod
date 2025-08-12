60eee4204f0a75f2a68a43b9df23e607f6d5cdf2
BlendyDev <github@blendy.dev>
2025-08-12 08:14:29 +0200

    fix: actually make signpaste work

--- src/main/java/me/vinzy/vmod/utils/ModuleManager.java
+++ src/main/java/me/vinzy/vmod/utils/ModuleManager.java
@@ -1,6 +1,5 @@
 package me.vinzy.vmod.utils;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import me.vinzy.vmod.VMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
@@ -19,53 +18,53 @@ import java.lang.reflect.Field;
 public class ModuleManager {
 
     public static void handleKeybinding (int key) {
+        handleModuleToggle(key);
+        if(key == Keyboard.KEY_NONE) return;
+        if (key == Keyboard.KEY_V && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+            GuiScreen currentScreen = Minecraft.getMinecraft().currentScreen;
+            if (currentScreen instanceof GuiEditSign) {
+                GuiEditSign signGui = (GuiEditSign) currentScreen;
+                try {
+                    Field titleSignField = signGui.getClass().getDeclaredField("field_146848_f"); //tileSign
+                    Field editLineField = signGui.getClass().getDeclaredField("field_146851_h"); //editLine
+                    titleSignField.setAccessible(true);
+                    editLineField.setAccessible(true);
+                    String lineContent = ((TileEntitySign) titleSignField.get(signGui)).signText[(int) editLineField.get(signGui)].getFormattedText();
+                    String clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+                    ((TileEntitySign) titleSignField.get(signGui)).signText[(int) editLineField.get(signGui)] = new ChatComponentText(lineContent + clipboard);
+                } catch (NoSuchFieldException | IllegalAccessException | IOException |
+                         UnsupportedFlavorException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+
+    }
+
+    public static void handleModuleToggle(int key) {
         GuiScreen gui = Minecraft.getMinecraft().currentScreen;
         if (!(gui instanceof GuiContainer || gui == null )) return;
-        if(key == Keyboard.KEY_NONE) return;
         if (key == ConfigFile.blink_Key) {
             if (VMod.toggleBlinkButton == null) {
                 return;
             }
             PacketUtils.toggleBlink();
-        } else if (key == ConfigFile.saveGui_Key) {
+        } if (key == ConfigFile.saveGui_Key) {
             GuiUtils.saveAndCloseScreen();
-        } else if (key == ConfigFile.ghostBlock_Key) {
+        } if (key == ConfigFile.ghostBlock_Key) {
             GhostBlock.deleteblock();
-        } else if (key == ConfigFile.restoreGhosts_Key) {
+        } if (key == ConfigFile.restoreGhosts_Key) {
             GhostBlock.restoreghosts();
-        } else if (key == ConfigFile.reopenGui_Key){
+        } if (key == ConfigFile.reopenGui_Key){
             GuiUtils.displayGUI();
-        } else if (key == ConfigFile.toggleTouch_Key) {
+        } if (key == ConfigFile.toggleTouch_Key) {
             Minecraft.getMinecraft().gameSettings.touchscreen = !Minecraft.getMinecraft().gameSettings.touchscreen;
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
                     VMod.PREFIX + "Toggled touchscreen."));
-        } else if (key == ConfigFile.printNbt_Key) {
+        } if (key == ConfigFile.printNbt_Key) {
             GuiUtils.printNBT();
-        } else if (key == Keyboard.KEY_V && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
-            GuiScreen currentScreen = Minecraft.getMinecraft().currentScreen;
-            if (currentScreen instanceof GuiEditSign) {
-                GuiEditSign signGui = (GuiEditSign) currentScreen;
-                try {
-                    Field titleSignField = signGui.getClass().getDeclaredField("tileSign");
-                    Field editLineField = signGui.getClass().getDeclaredField("editLine");
-                    titleSignField.setAccessible(true);
-                    editLineField.setAccessible(true);
-                    String lineContent = ((TileEntitySign)titleSignField.get(signGui)).signText[(int)editLineField.get(signGui)].getFormattedText();
-                    String clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
-                    ((TileEntitySign)titleSignField.get(signGui)).signText[(int)editLineField.get(signGui)] = new ChatComponentText(lineContent+clipboard);
-                } catch (NoSuchFieldException e) {
-                    throw new RuntimeException(e);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (UnsupportedFlavorException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
-
     }
-
 }
 
